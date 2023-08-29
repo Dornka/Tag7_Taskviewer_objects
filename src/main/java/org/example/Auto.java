3720ae995e94413fb8729056ea6443b61c781496
Dornka <Skiber@gmail.com>
2023-08-29 11:51:06 +0200

    Klasse und Methoden erstellt/verwendet

--- src/main/java/org/example/Auto.java
+++ src/main/java/org/example/Auto.java
@@ -1,2 +1,46 @@
-package org.example;public class Auto {
+package org.example;
+
+public class Auto {
+
+    private String marke;
+    private String modell;
+    private String farbe;
+    private int baujahr;
+    private boolean gestartet;
+    private int geschwindigkeit;
+
+
+    public Auto(String marke, String modell, String farbe, int baujahr){
+        this.marke = marke;
+        this.modell = modell;
+        this.farbe = farbe;
+        this.baujahr = baujahr;
+        this.geschwindigkeit = 0;
+        this.gestartet = false;
+    }
+
+    public Auto() {
+
+    }
+
+    public void starten() {
+
+        if(!gestartet) {
+            gestartet = true;
+            System.out.println("Das Auto wurde gestartet.");
+        } else {
+            System.out.println("Das Auto ist bereits gestartet.");
+        }
+
+    }
+    public void beschleunigen(int tempo) {
+
+        if (gestartet){
+            geschwindigkeit = geschwindigkeit + tempo;
+            System.out.println("Auto beschleunigt um " + tempo + " km/h. " + "Neue Geschwindigkeit: " + geschwindigkeit + " km/h.");
+        } else {
+            System.out.println("Bitte zuerst Auto starten, bevor es beschleunigen kann.");
+        }
+
+    }
 }
